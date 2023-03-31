565712b97123f588c1a29ca5e2f83e8b116f00c1
Nata R <natasha.melanis@yandex.ru>
2023-03-31 13:42:16 +0300

    BonusServiceTest class added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         BonusService service = new BonusService();
-        long bonus = service.calculate(1_000, true);
-        System.out.println(bonus);
+
+        long expected = 30;
+        long actual = service.calculate(1_000, true);
+        System.out.println(expected + " == ? == " + actual);
     }
 }
